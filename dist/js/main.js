030b8c37ac219de32f24d9a1872fb3c4470d194f
yesRedream <yesredream@gmail.com>
2022-09-30 17:11:14 +0300

    text update

--- dist/js/main.js
+++ dist/js/main.js
@@ -260,26 +260,26 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()(".page-rating-slider").slick({
   infinite: true,
   arrows: false,
   dots: false,
-  autoplay: false,
-  slidesToShow: 5,
+  autoplay: true,
+  slidesToShow: 4,
   slidesToScroll: 1,
   lazyLoad: 'progressive',
   responsive: [{
     breakpoint: 1224,
     settings: {
-      slidesToShow: 4,
+      slidesToShow: 3,
       slidesToScroll: 1
     }
   }, {
     breakpoint: 900,
     settings: {
-      slidesToShow: 3,
+      slidesToShow: 2,
       slidesToScroll: 1
     }
   }, {
     breakpoint: 700,
     settings: {
-      slidesToShow: 2,
+      slidesToShow: 1,
       slidesToScroll: 1
     }
   }, {
